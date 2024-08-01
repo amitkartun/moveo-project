0aef7015c672893bb6d4e3330c13677c7700c7f0
amitkartun <amitkartun150198@gmail.com>
2024-08-01 18:11:27 +0300

    Added deployment branch changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 const codeBlockRoutes = require('./routes/codeBlockRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const socketManager = require('./websockets/socketManager');
@@ -20,14 +21,22 @@ app.use(function(req, res, next) {
 
 app.use(express.json());
 
+// Serve static files from the React frontend app 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
 app.use('/api/codeblocks', codeBlockRoutes);
 
 // WebSocket setup
 socketManager(io);
 
+// After your routes, add this block to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Error handling middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT; 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
